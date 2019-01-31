a68b06b36ae16f3d36b1bd7c3e4e289f00d2ca30
Marcio Lourenco <xquema@gmail.com>
2019-01-30 23:11:20 -0200

    deploy

---
+++ precache-manifest.65ae3a756f298f8ae22afdcb8242d3ef.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vue-admin-template/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-admin-template/robots.txt"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vue-admin-template/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c5f0e06d3befb820b07f",
+    "url": "/vue-admin-template/js/app.ef71f80a.js"
+  },
+  {
+    "revision": "9e5719ab57f04c0d6341",
+    "url": "/vue-admin-template/js/chunk-2d0d63f1.115bf18e.js"
+  },
+  {
+    "revision": "3261581f06a701866af8",
+    "url": "/vue-admin-template/js/chunk-27728818.d661ee0a.js"
+  },
+  {
+    "revision": "d3f25e57b4546d1147c6",
+    "url": "/vue-admin-template/js/chunk-vendors.66eec9ea.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-admin-template/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d78db66c369ba23f7a32",
+    "url": "/vue-admin-template/js/about.fc573c4e.js"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/vue-admin-template/fonts/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vue-admin-template/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "bb1fd77d2c716e52997b511e64a8c7a0",
+    "url": "/vue-admin-template/index.html"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "/vue-admin-template/img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vue-admin-template/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vue-admin-template/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/vue-admin-template/fonts/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "55242ea5c378da626cab5b4aaf9c2255",
+    "url": "/vue-admin-template/fonts/MaterialIcons-Regular.55242ea5.ttf"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/vue-admin-template/fonts/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "d3f25e57b4546d1147c6",
+    "url": "/vue-admin-template/css/chunk-vendors.3ba03f92.css"
+  },
+  {
+    "revision": "c5f0e06d3befb820b07f",
+    "url": "/vue-admin-template/css/app.ff31d59d.css"
+  }
+];
